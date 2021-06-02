087d5c4bcaf7a5cab7383af059619938dc905304
deepakmench18 <deepakmench18@gmail.com>
2021-06-02 19:56:20 +0530

    Daily wages with total wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,32 +4,26 @@ totalSalary=0
 empRatePerHr=20
 numWorkingDays=0
 hrcheck=0
-getHr() {
-   empcheck=$1
-   case $empcheck in
-
-     $isfullTime)
-        empHr=8
-        ;;
-
-     $isPartTime)
-        empHr=4
-        ;;
-        *)
-           empHr=0
-           ;;
-   esac
-}
-while [ $hrcheck -lt 100 ] || [  $numWorkingDays -lt 20 ]
+while [ $hrcheck -le 100 ] || [  $numWorkingDays -le 20 ]
 do
-        empCheck=$((RANDOM%3))
-         getHr $empCheck
-
-      ((numWorkingDays++))
+        empCheck=$((RANDOM%3));
+                case $empCheck in
+                        $isfullTime)
+                                empHr=8
+                                ;;
+                        $isPartTime)
+                                empHr=4
+                                ;;
+                        *)
+                        empHr=0
+                                ;;
+                esac
       hrcheck=$(($hrcheck+$empHr))
       salary=$(($empHr*$empRatePerHr))
+      Dailywage[$numWorkingDays]=$salary
       totalSalary=$(($totalSalary+$salary))
-
+       totalwage[$numWorkingDays]=$totalSalary
+      ((numWorkingDays++))
 done
-echo "Total salay ="$totalSalary
-echo "Emlpoyee working days = $numWorkingDays and hours = $hrcheck"
+echo "Total salay ="${totalwage[@]}
+echo "Daily salary="${Dailywage[@]}
