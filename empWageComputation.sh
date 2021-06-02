2ba0b15e9951caf8e31ac3ad16c1f3c3f6686791
deepakmench18 <deepakmench18@gmail.com>
2021-06-02 19:47:42 +0530

    Function get work hour

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,26 +4,32 @@ totalSalary=0
 empRatePerHr=20
 numWorkingDays=0
 hrcheck=0
-while [ $hrcheck -le 100 ] || [  $numWorkingDays -le 20 ]
-do
-        empCheck=$((RANDOM%3));
-                case $empCheck in
-                        $isfullTime)
-                                empHr=8
-                                ;;
-                        $isPartTime)
-                                empHr=4
-                                ;;
-                        *)
-                        empHr=0
-                                ;;
-                esac
+getHr() {
+   empcheck=$1
+   case $empcheck in
 
-    ((numWorkingDays++))
+     $isfullTime)
+        empHr=8
+        ;;
+
+     $isPartTime)
+        empHr=4
+        ;;
+        *)
+           empHr=0
+           ;;
+   esac
+}
+while [ $hrcheck -lt 100 ] || [  $numWorkingDays -lt 20 ]
+do
+        empCheck=$((RANDOM%3))
+         getHr $empCheck
+
+      ((numWorkingDays++))
       hrcheck=$(($hrcheck+$empHr))
       salary=$(($empHr*$empRatePerHr))
       totalSalary=$(($totalSalary+$salary))
 
 done
 echo "Total salay ="$totalSalary
-echo "Emlpoyee working days = $numWorkingDays  &  hours = $hrcheck "
+echo "Emlpoyee working days = $numWorkingDays and hours = $hrcheck"
