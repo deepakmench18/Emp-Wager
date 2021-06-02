25f4b65f647bc149053e6d5bf85f0f0fa64032ae
deepakmench18 <deepakmench18@gmail.com>
2021-06-02 18:06:48 +0530

    Part time Employee & wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,21 @@
-check=$((RANDOM%2))
+isFulltime=1
+isParttime=2
+PartTimeWage=0
+FullDayWage=0
 WageprHr=20
-
-if [ $check -eq 1 ]
+check=$(shuf -i 0-2 -n 1)
+if [ $check -eq $isFulltime ]
 then
    FullDayHr=8
-   DailyWage=$(($WageprHr*FullDayHr))
-   echo "Employee per Day wage =" $DailyWage
+   FullDayWage=$(($FullDayHr*$WageprHr))
+
+   echo "Full Day Wage =" $FullDayWage
+
+elif [ $check -eq $isParttime ]
+then
+   PartTimeHr=8
+   PartTimeWage=$(($WageprHr*$PartTimeHr))
+   echo "Part Time Wage =" $PartTimeWage
 else
    echo "Employee is Absent"
 fi
